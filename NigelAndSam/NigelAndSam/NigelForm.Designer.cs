6152ef7b01508e17bae963fc710de1e939aea707
Nigel Edwards <253737@col-westanglia.ac.uk>
2020-09-21 12:13:16 +0100

    Added my form

--- NigelAndSam/NigelAndSam/NigelForm.Designer.cs
+++ NigelAndSam/NigelAndSam/NigelForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NigelAndSam
 {
-    partial class Form1
+    partial class NigelForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,46 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_sam = new System.Windows.Forms.Button();
-            this.btn_nigel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btn_sam
+            // label1
             // 
-            this.btn_sam.Location = new System.Drawing.Point(13, 13);
-            this.btn_sam.Name = "btn_sam";
-            this.btn_sam.Size = new System.Drawing.Size(75, 23);
-            this.btn_sam.TabIndex = 0;
-            this.btn_sam.Text = "Sam";
-            this.btn_sam.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(209, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nigel\'s Form";
             // 
-            // btn_nigel
-            // 
-            this.btn_nigel.Location = new System.Drawing.Point(13, 43);
-            this.btn_nigel.Name = "btn_nigel";
-            this.btn_nigel.Size = new System.Drawing.Size(75, 23);
-            this.btn_nigel.TabIndex = 1;
-            this.btn_nigel.Text = "Nigel";
-            this.btn_nigel.UseVisualStyleBackColor = true;
-            this.btn_nigel.Click += new System.EventHandler(this.btn_nigel_Click);
-            // 
-            // Form1
+            // NigelForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btn_nigel);
-            this.Controls.Add(this.btn_sam);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Name = "NigelForm";
+            this.Text = "NigelForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btn_sam;
-        private System.Windows.Forms.Button btn_nigel;
+        private System.Windows.Forms.Label label1;
     }
 }
-
