fdab7df46f111291e109e163daa3f20041f91ab9
Nigel Edwards <253737@col-westanglia.ac.uk>
2020-09-21 11:28:20 +0100

    Add my float

--- NigelAndSam/NigelAndSam/Nigel.cs
+++ NigelAndSam/NigelAndSam/Nigel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,6 @@ namespace NigelAndSam
     class Nigel
     {
         private int m_nigel = 7;
+        private float m_nigel2 = 3.2F;
     }
 }
